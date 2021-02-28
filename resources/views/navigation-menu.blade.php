a6553c25633854f838a85362e060c031be7432b1
Mohamed <mohamedelyousfi87@gmail.com>
2021-02-28 15:27:57 +0100

    Fixed missing element in responsive navbar

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -144,6 +144,9 @@
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-jet-responsive-nav-link>
+            <x-jet-responsive-nav-link href="{{ route('queue') }}" :active="request()->routeIs('queue')">
+                Wachtlijst
+            </x-jet-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
